c5f54261f83fccdacb88770271257f55478b9619
antoanelenkov <antoanelenkov@gmail.com>
2015-09-07 17:48:28 +0300

    half-ready

--- JS-Apps-Exam-prep/forum-workshop/public/scripts/data.js
+++ JS-Apps-Exam-prep/forum-workshop/public/scripts/data.js
@@ -1,16 +1,23 @@
 var data = (function () {
+    var USER_AUTH_KEY_HEADER,
+        USERNAME,
+        storage = localStorage;
     //---------------POSTS--------------------
-    function getAllPosts() {
+    function getAllPosts(query) {
+
+
         var promise = new Promise(function (resolve, reject) {
             $.ajax({
                 url: 'api/threads',
-                type: 'get',
+                type: 'GET',
+                data:query,
                 contentType: 'application/json',
                 success: function (data) {
                     resolve(data);
                 },
-                error: function(err){
-                    console.log('error while loading all posts: '+err)
+                error: function (err) {
+                    alert('error while loading all posts: ' + err.responseText);
+                    reject();
                 }
             });
         })
@@ -22,26 +29,110 @@ var data = (function () {
 
     }
 
-    function addPost() {
+    function addPost(post) {
+        console.log('adding post header: '+storage.getItem(USER_AUTH_KEY_HEADER))
+        console.log('adding post username: '+storage.getItem(USERNAME))
+        var promise = new Promise(function (resolve, reject) {
+            $.ajax({
+                url: 'api/threads',
+                type: 'POST',
+                data:JSON.stringify(post),
+                headers:{
+                    'x-authkey':storage.getItem(USERNAME)
+                },
+                contentType: 'application/json',
+                success: function (data) {
+                    resolve(data);
+                },
+                error: function (err) {
+                    alert('error while creating new post: ' + err.responseText);
+                    reject();
+                }
+            });
+        })
 
+        return promise;
     }
 
     function addMessageToPostWithId() {
-
     }
 
 
     //---------------USERS--------------------
-    function registerUser() {
+    function registerUser(user) {
+        var userToSend = {
+            username: user.username,
+            passHash: CryptoJS.SHA1(user.password).toString()
+        };
 
+        var promise = new Promise(function (resolve, reject) {
+            $.ajax({
+                url: 'api/users',
+                type: 'POST',
+                data: JSON.stringify(userToSend),
+                contentType: 'application/json',
+                success: function (data) {
+                    resolve(data);
+                },
+                error: function (err) {
+                    alert('Error occured when registering user: ' + err.responseText);
+                }
+            });
+        })
+
+        return promise;
     }
 
-    function loginUser() {
+    function loginUser(user) {
+        var userToSend = {
+            username: user.username,
+            passHash: CryptoJS.SHA1(user.password).toString()
+        };
 
+        var promise = new Promise(function (resolve, reject) {
+            $.ajax({
+                url: 'api/auth',
+                type: 'PUT',
+                data: JSON.stringify(userToSend),
+                contentType: 'application/json',
+                success: function (data) {
+                    storage.setItem(USERNAME,data.username);
+                    storage.setItem(USER_AUTH_KEY_HEADER,data.authKey);
+                    resolve();
+                },
+                error: function (err) {
+                    alert('Error occured when logging user: ' + err.responseText);
+                }
+            });
+        })
+
+        return promise;
     }
 
-    function getCurrentUser(){
-        return null;
+    function getCurrentUser() {
+        var promise = new Promise(function (resolve, reject) {
+            if (storage.getItem(USER_AUTH_KEY_HEADER) === null) {
+                console.log('unlogged')
+                resolve(null);
+
+            }
+            else {
+                console.log('logged');
+                resolve(storage.getItem(USERNAME));
+            }
+        })
+
+        return promise;
+    }
+
+    function logoutUser() {
+        var promise=new Promise(function(resolve,reject){
+            storage.removeItem(USER_AUTH_KEY_HEADER);
+            storage.removeItem(USERNAME);
+            resolve();
+        })
+
+        return promise;
     }
 
     function getAllUsers() {
@@ -59,7 +150,8 @@ var data = (function () {
             register: registerUser,
             login: loginUser,
             getAll: getAllUsers,
-            getCurrent:getCurrentUser
+            getCurrent: getCurrentUser,
+            logout: logoutUser
         }
     }
 }());
--- JS-Apps-Exam-prep/forum-workshop/public/scripts/index.js
+++ JS-Apps-Exam-prep/forum-workshop/public/scripts/index.js
@@ -1,74 +1,186 @@
 var sammy = Sammy('#content', function () {
-    var $content = $('#content'),
-        $regBtn,
-        $loginBtn;
+    var $content = $('#content');
+    var user = {},
+        that = this;
 
-    this.get('#/', function (context) {
-        var currentUser=data.users.getCurrent();
+    renderLoginAndLogoutButton();
 
-        if(currentUser===null){
-            context.redirect('#/login');
-        }
-        else{
-            context.redirect('#/threads');
-        }
+    var $loginBtn = $("#threads-login-btn"),
+        $logoutBtn = $("#threads-logout-btn");
 
-        console.log('inital page');
+    $logoutBtn.on('click', function () {
+        data.users.logout()
+            .then(function () {
+                console.log('in logout btn');
+                renderLoginAndLogoutButton();
+                window.location.hash = ('#/threads');
+
+            })
+    });
+
+    $loginBtn.on('click', function () {
+        renderLoginAndLogoutButton();
+        window.location.hash = ('#/login');
+    });
+
+
+    function renderLoginAndLogoutButton() {
+        data.users.getCurrent()
+            .then(function (user) {
+                if (user === null) {
+                    $loginBtn.removeClass('hidden');
+                    $logoutBtn.addClass('hidden');
+                }
+                else {
+                    $loginBtn.addClass('hidden');
+                    $logoutBtn.removeClass('hidden');
+                }
+            });
+    }
+
+
+    that.get('#/', function (context) {
+        context.redirect('#/threads');
     })
 
-    this.get('#/login', function (context) {
+    that.get('#/login', function (context) {
         templates.get('login')
             .then(function (source) {
                 $content.html(source);
             })
             .then(function () {
-                $regBtn = $('#btn-register');
-                $regBtn.on('click', function () {
+                var $regRedirectBtn = $('#btn-redirect-register');
+                var $btnLogin = $('#btn-login');
+
+                $regRedirectBtn.on('click', function () {
                     context.redirect('#/register');
                 })
+
+                $btnLogin.on('click', function () {
+                    user.username = $('#tb-username-login').val();
+                    user.password = $('#tb-password-login').val();
+
+                    data.users.login(user)
+                        .then(function () {
+                            renderLoginAndLogoutButton();
+                        })
+                        .then(function () {
+                            context.redirect('#/threads');
+                        });
+                })
             })
-
-
-        console.log('login page');
     })
 
-    this.get('#/register', function () {
+    that.get('#/register', function (context) {
         templates.get('register')
             .then(function (source) {
                 $content.html(source);
             })
+            .then(function () {
+                var $regBtn = $('#btn-register');
 
-        console.log('register page');
+                $regBtn.on('click', function () {
+                    user.username = $('#tb-username-register').val();
+                    user.password = $('#tb-password-register').val();
+
+                    data.users.register(user)
+                        .then(function () {
+                            renderLoginAndLogoutButton();
+                        })
+                        .then(function () {
+                            context.redirect('#/threads');
+                            alert('You are registred, please log in with your username and password');
+                        })
+                })
+            })
     })
 
-    this.get('#/threads', function () {
+    var query = {
+        page: 0,
+        size: 10
+    }
+
+    that.get('#/threads', function (context) {
         var threads;
-        data.posts.getAll()
+
+
+        data.posts.getAll(query)
             .then(function (respond) {
                 threads = respond.result;
-
                 return templates.get('threads');
             })
             .then(function (source) {
                 $content.html(source(threads));
-            });
+            })
+            .then(function () {
+                var $addPostBtn = $('#addThread-btn'),
+                    $addPostTitleTb = $('#threadTitle-tb'),
+                    $addPostMessageTb = $('#threadMessage-tb'),
+                    $previous = $('#previous-btn'),
+                    $next = $('#next-btn');
 
-        console.log('inital page');
+
+                $previous.on('click', function () {
+                    query.page -= 1;
+                    console.log('previous')
+                    data.posts.getAll(query)
+                        .then(function (respond) {
+                            threads = respond.result;
+                            return templates.get('threads');
+                        })
+                        .then(function (source) {
+                            $content.html(source(threads));
+                        })
+                        .then(function(){
+                            context.redirect('#/');
+                        })
+                })
+
+                $next.on('click', function () {
+                    query.page += 1;
+                    console.log('next')
+                    data.posts.getAll(query)
+                        .then(function (respond) {
+                            threads = respond.result;
+                            return templates.get('threads');
+                        })
+                        .then(function (source) {
+                            $content.html(source(threads));
+                        })
+                        .then(function(){
+                            context.redirect('#/');
+                        })
+                })
+
+                $addPostBtn.on('click', function () {
+                    var post = {
+                        title: $addPostTitleTb.val(),
+                        message: $addPostMessageTb.val()
+                    }
+
+                    data.posts.addPost(post)
+                        .then(function () {
+                            context.redirect('#/');
+                        })
+
+                })
+            })
+
     })
 
-    this.get('#/threads/add', function () {
+    that.get('#/threads/add', function () {
         console.log('add post');
     })
 
-    this.get('#/threads/:id', function () {
+    that.get('#/threads/:id', function () {
         console.log('get post by id');
     })
 
-    this.get('#/threads/:id/messages/add', function () {
+    that.get('#/threads/:id/messages/add', function () {
         console.log('add message to post with certain id');
     })
 
-});
+})
 
 $(function () {
     sammy.run('#/');
