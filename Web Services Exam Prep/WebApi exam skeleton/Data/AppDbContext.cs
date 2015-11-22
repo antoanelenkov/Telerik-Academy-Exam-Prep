0074b610d83b01bc501be85d8f59aa1ff9e6f4e3
antoanelenkov <antoanelenkov@gmail.com>
2015-11-22 15:23:45 +0200

    initial skeleton

---
+++ Web Services Exam Prep/WebApi exam skeleton/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public AppDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static AppDbContext Create()
+        {
+            return new AppDbContext();
+        }
+    }
+}
